c8c497604c2a39438f289b17ba2118385ec0dc1d
jaysa <gh@jaysa.net>
2025-07-14 23:49:56 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+	  pkgs.python313Packages.mkdocs-material
+        ];
+      };
+      formatter.${system} = pkgs.nixpkgs-fmt;
+    };
+}
+
+# nix flake init, then 
+# nix fmt
+# nix develop
