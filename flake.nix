2489706604bd559d00f98cf80bf65ef3a8101620
Oliver Ni <oliverni@berkeley.edu>
2025-07-19 10:55:18 +0800

    feat: support more systems on flake

--- flake.nix
+++ flake.nix
@@ -3,26 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, systems }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+      forAllSystems = fn: nixpkgs.lib.genAttrs (import systems) (system: fn (pkgsFor system));
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs.python313Packages; [
-	  mkdocs-material
-	  mkdocs-rss-plugin
-	  mkdocs-git-revision-date-localized-plugin
-        ];
-	shellHook = "mkdocs serve";
-      };
-      formatter.${system} = pkgs.nixpkgs-fmt;
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs.python313Packages; [
+            mkdocs-material
+            mkdocs-rss-plugin
+            mkdocs-git-revision-date-localized-plugin
+          ];
+          shellHook = "mkdocs serve";
+        };
+      });
+
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
     };
 }
 
-# nix flake init, then 
+# nix flake init, then
 # nix fmt
 # nix develop
