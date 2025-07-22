3ebb48a42f91128104bc940408d5a88d363879f9
Oliver Ni <oliver.ni@gmail.com>
2025-07-22 01:28:54 -0400

    feat: support `nix build` and refactor flake (#2)

--- flake.nix
+++ flake.nix
@@ -3,14 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, systems }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+      forAllSystems = fn: nixpkgs.lib.genAttrs (import systems) (system: fn (pkgsFor system));
     in
     {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./. { };
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${pkgs.system}.default ];
+          shellHook = "mkdocs serve";
+        };
+      });
+
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs.python313Packages; [
 	  mkdocs-material
@@ -23,7 +36,3 @@
       formatter.${system} = pkgs.nixpkgs-fmt;
     };
 }
-
-# nix flake init, then 
-# nix fmt
-# nix develop
